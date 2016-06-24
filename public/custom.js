df6d689e350f2480595f3bfde6f220a86c23cbc0
Tao Ning <ningtaous@gmail.com>
2016-06-23 23:41:54 -0400

    feat: implement feeling lucky button

--- public/custom.js
+++ public/custom.js
@@ -1,17 +1,51 @@
-$(function(){
+$(function() {
+  // === Begin state of the lucky button ===
+  var isLuckySelected = false;
+
+  $('#where-are-you-going-input').keyup(function() {
+    if ($(this).val() !== '') {
+      isLuckySelected = true;
+      $('#hotel-search-form-lucky-submit-btn').text('I\'m Feeling Lucky');
+    } else {
+      isLuckySelected = false;
+      $('#hotel-search-form-lucky-submit-btn').html('I\'m Feeling... <span class="glyphicon glyphicon-menu-down" style="float:right"></span>');
+    }
+  })
+
+  $('#hotel-search-form-lucky-submit-btn').hover(function() {
+    if (isLuckySelected) {
+      $('div.dropdown').removeClass('open');
+    } else {
+      $('div.dropdown').addClass('open');
+    }
+  }, function() {
+    // Do nothing in hover out.
+  });
+
+  $('#hotel-search-form-lucky-submit-btn').click(function() {
+    if (isLuckySelected) {
+      $('div.dropdown').removeClass('open');
+      console.log('TODO Submit');
+    }
+  });
+
+  $('.feeling-option').click(function() {
+    isLuckySelected = true;
+    var btnInfo = 'I\'m Feeling ' + $(this).text();
+    $('#hotel-search-form-lucky-submit-btn').text(btnInfo);
+  });
+
+  // === End state of the lucky button ===
+
   // Hide the opening picker when user scroll
-  $(window).scroll(function(){
+  $(window).scroll(function() {
     $('div.dropdown').removeClass('open');
     $('#ui-datepicker-div').hide();
   });
 
-  $('#hotel-search-form-lucky-submit-btn').hover(function(){
-    $('div.dropdown').addClass('open');
-  }, function(){
-    // Do nothing in hover out.
-  });
+  $('#check-in-input').datepicker();
+  $('#check-out-input').datepicker();
 
-  $( '#check-in-input' ).datepicker();
-  $( '#check-out-input' ).datepicker();
+
 });
 
